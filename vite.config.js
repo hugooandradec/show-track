ec7c4883d1734f4a4652b85679818f0ce3bad486
Victor Hugo <96561669+hugooandradec@users.noreply.github.com>
2026-03-19 08:08:23 -0300

    Update base path in Vite configuration
    Set the base path for the Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/show-track/", // 🔥 ISSO AQUI É O SEGREDO
   plugins: [react(), tailwindcss()],
 })
